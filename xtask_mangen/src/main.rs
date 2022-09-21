bca9b989edda9c0ab1f51a6b7ed077deeb0bd695
Fred Brennan <copypaste@kittens.ph>
2022-09-21 01:59:47 -0400

    Tried to use clap-rs/clap@16c46b4 to make a manpage, but it was sadly not up to par :(
    <https://github.com/clap-rs/clap/tree/master/clap_mangen>

---
+++ xtask_mangen/src/main.rs
@@ -0,0 +1,17 @@
+use clap;
+use clap::{IntoApp, Parser};
+use humnum;
+
+fn main() -> std::io::Result<()> {
+    let out_dir = std::path::PathBuf::from(std::env::var_os("OUT_DIR").ok_or_else(|| std::io::ErrorKind::NotFound)?);
+
+    let cmd = <humnum::Options as clap::CommandFactory>::command();
+
+    let man = clap_mangen::Man::new(cmd);
+    let mut buffer: Vec<u8> = Default::default();
+    man.render(&mut buffer)?;
+
+    std::fs::write(out_dir.join("hns.1"), buffer)?;
+
+    Ok(())
+}
